9d3acbdfdf71af3d3a26cf8373744c57c2287bfe
Alex Chan <alexc197@gmail.com>
2019-01-27 17:34:38 -0500

    created button layout

---
+++ RatingControl.swift
@@ -0,0 +1,54 @@
+//
+//  RatingControl.swift
+//  MealTracker
+//
+//  Created by Lian, Jonathan on 1/27/19.
+//  Copyright © 2019 Lian, Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            ratingButtons.append(button)
+        }
+    }
+    
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+}
