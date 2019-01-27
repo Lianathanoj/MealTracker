b6a197d5e05b81c92eb9fc7563d6d43cbbcde96f
Jonathan Lian <jlian@gatech.edu>
2019-01-27 13:57:55 -0500

    set default label text via target-action pattern

--- MovieTracker/ViewController.swift
+++ MovieTracker/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var movieNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        movieNameLabel.text = "Default Text"
+    }
 }
 
