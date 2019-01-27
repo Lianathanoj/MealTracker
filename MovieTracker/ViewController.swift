dde410ea7a10d363c6fc5b6858c01afbe5675e43
Lian, Jonathan <jlian9@mstudio08.local>
2019-01-27 12:51:36 -0500

    Initial Commit

---
+++ MovieTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieTracker
+//
+//  Created by Lian, Jonathan on 1/27/19.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
